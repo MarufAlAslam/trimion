b19fb865f4fd2b2880f1b425ef4a18884d489e7e
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-09-10 04:10:06 +0600

    desktop view done

--- js/app.js
+++ js/app.js
@@ -51,3 +51,29 @@ $('.slider-nav').slick({
     centerMode: true,
     focusOnSelect: true
 });
+
+
+$('.fieldBtn').click(function () {
+    $('.fieldBtn').removeClass('active');
+    $(this).addClass('active');
+    $('.btn-field').hide(100)
+})
+
+$('.btn1').click(function () {
+    $('.btn1Field').show(100)
+})
+$('.btn2').click(function () {
+    $('.btn2Field').show(100)
+})
+$('.btn3').click(function () {
+    $('.btn3Field').show(100)
+})
+$('.btn4').click(function () {
+    $('.btn4Field').show(100)
+})
+$('.btn5').click(function () {
+    $('.btn5Field').show(100)
+})
+$('.btn6').click(function () {
+    $('.btn6Field').show(100)
+})
